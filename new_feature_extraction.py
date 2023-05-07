9701f58f8d8df6e2007ad7fa21ac0b363a9aa428
Rithik Pothuganti <pothugan@usc.edu>
2023-05-07 00:57:44 -0700

    softmax neural network

--- new_feature_extraction.py
+++ new_feature_extraction.py
@@ -4,6 +4,23 @@ import numpy as np
 STOCKS = ["aapl", "amd", "amzn", "googl", "meta", "msft", "nflx", "nvda", "qcom", "tsla"]
 DIR = "/Users/rithikpothuganti/cs467/csci467-project/data"
 
+def assign_label(row):
+    if row['target_return'] <= -0.1:
+        # strong sell
+        return 0
+    elif -0.1 < row['target_return'] <= -0.05:
+        # underweight
+        return 1
+    elif -0.05 < row['target_return'] <= 0.05:
+        # neutral
+        return 2
+    elif 0.05 < row['target_return'] <= 0.1:
+        # overweight
+        return 3
+    else:
+        # strong buy
+        return 4
+
 def get_stock_df(file_path):
     df = pd.read_csv(file_path)
     df = df[::-1].reset_index()
@@ -22,10 +39,12 @@ def get_stock_df(file_path):
     # Create a label column with the target return and buy/sell signal
     df['target_return'] = df['Close/Last'].pct_change(255).shift(-255)
     df['label'] = df.apply(lambda row: (1 if row['target_return'] > 0 else 0), axis=1)
+    df['rating'] = df.apply(assign_label, axis=1)
+
 
     # Select the relevant columns for the training examples
     features = ['r_90d', 'r_180d', 'r_1y', 'r_2y', 'r_5y', 'r_10y']
-    label = ['target_return', 'label', 'Date', 'Close/Last']
+    label = ['target_return', 'label', 'rating', 'Date', 'Close/Last']
     df_ml = df.loc[255:, features + label].dropna(subset=['target_return'])
     return df_ml
 
@@ -50,6 +69,9 @@ def read_data(X_df, Y_df):
 
 def read_logistic_data(X_df, Y_df):
     return X_df.to_numpy(), Y_df['label'].to_numpy()
+
+def read_buy_rating_data(X_df, Y_df):
+    return X_df.to_numpy(), Y_df['rating'].to_numpy()
 
 if __name__ == "__main__":
     df = get_all_stocks_df()
